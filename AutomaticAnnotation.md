41bec9998cbba0a6f3b71b8e951ea41b5076898d
Xuan Hien <hiennguyen9874@gmail.com>
2025-06-30 10:51:36 +0000

    init cvt deploy project

---
+++ AutomaticAnnotation.md
@@ -0,0 +1,29 @@
+# Integrate custom YOLOv8 && YOLO11 model into CVAT for automatic annotation
+
+## Start
+
+1. Start CVAT together with the plugin use for AI automatic annotation assistant.
+
+```bash
+docker compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml up -d
+```
+
+2. Install nuctl
+
+```bash
+wget https://github.com/nuclio/nuclio/releases/download/1.14.8/nuctl-1.14.8-linux-amd64
+```
+
+3. After downloading the nuclio, give it a proper permission and do a softlink
+
+```bash
+sudo chmod +x nuctl-1.14.8-linux-amd64
+sudo ln -sf $(pwd)/nuctl-1.14.8-linux-amd64 /usr/local/bin/nuctl
+```
+
+4. Build the docker image and run the container. After it is done, you can use the model right away in the CVAT.
+   ```
+   DOCKER_BUILDKIT=1 ./serverless/deploy_gpu.sh yolov8
+   ```
+
+## Stop
---
+++ settings/production.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2018-2022 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+# Inherit parent config
+from .base import *  # pylint: disable=wildcard-import
+
+DEBUG = False
+
+NUCLIO["HOST"] = os.getenv("CVAT_NUCLIO_HOST", "nuclio")
+
+# Django-sendfile:
+# https://github.com/moggers87/django-sendfile2
+SENDFILE_BACKEND = "django_sendfile.backends.nginx"
+SENDFILE_URL = "/"
+
+LOGGING["formatters"]["verbose_uvicorn_access"] = {
+    "()": "uvicorn.logging.AccessFormatter",
+    "format": '[{asctime}] {levelprefix} {client_addr} - "{request_line}" {status_code}',
+    "style": "{",
+}
+LOGGING["handlers"]["verbose_uvicorn_access"] = {
+    "formatter": "verbose_uvicorn_access",
+    "class": "logging.StreamHandler",
+    "stream": "ext://sys.stdout",
+}
+LOGGING["loggers"]["uvicorn.access"] = {
+    "handlers": ["verbose_uvicorn_access"],
+    "level": "INFO",
+    "propagate": False,
+}
+CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGINS"), f"http://{CVAT_HOST}:8080"]
+STATIC_URL = os.getenv("STATIC_URL", "/static/")
